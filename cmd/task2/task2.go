c203cbd0e7eef2cd865386303b7e88b8fd86373b
Ronen Lapushner <ronenk17@gmail.com>
2021-10-19 15:26:38 +0300

    Changed server model to accomodate two servers

--- cmd/task2/task2.go
+++ cmd/task2/task2.go
@@ -1,32 +1,76 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/ronen25/task2/service"
 )
 
-func main() {
-	// Get server port
-	serverPort, exists := os.LookupEnv("SERVER_PORT")
-	if !exists {
-		log.Fatalln("Error: Environment variable `SERVER_PORT` is missing")
-	}
-
-	service := service.NewService()
+func httpServerRoutine(serverPort string, doneChannel <-chan os.Signal) {
+	service := service.NewHTTPService()
 
 	// Initialize router
 	router := mux.NewRouter()
 	router.Use(mux.CORSMethodMiddleware(router))
 	router.HandleFunc("/", service.QueryPrintingHandler)
 
+	// Listen on provided HTTP port
 	bindAddress := fmt.Sprintf(":%s", serverPort)
+	server := &http.Server{Addr: bindAddress, Handler: router}
+
 	log.Printf("Listening on address %s", bindAddress)
-	if err := http.ListenAndServe(bindAddress, router); err != nil {
-		log.Fatalf("Error setting up HTTP server: %v", err)
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("Error setting up HTTP server: %v", err)
+		}
+	}()
+
+	<-doneChannel
+
+	// Exit handler
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Error shutting down HTTP server: %v", err)
 	}
+}
+
+func grpcServerRoutine(serverPort string, doneChannel chan<- bool) {
+}
+
+func main() {
+	const SERVER_COUNT = 1
+	signalChannel := make(chan os.Signal, 1)
+
+	portVars := map[string]string{
+		"HTTP_PORT": "",
+		"GRPC_PORT": "",
+	}
+
+	// Get ports from the environment
+	for key := range portVars {
+		if value, exists := os.LookupEnv(key); exists {
+			portVars[key] = value
+		} else {
+			// Variable unavailable = error
+			log.Fatalf("Error: Cannot find required environment variable `%s`", key)
+		}
+	}
+
+	// Register SIGINT signal handler. We use this to quit the app properly.
+	signal.Notify(signalChannel, syscall.SIGINT, syscall.SIGTERM)
+
+	// Initialize both services - one on HTTP, the other on GRPC
+	go httpServerRoutine(portVars["HTTP_PORT"], signalChannel)
+
+	// App quits when both servers are done
+	<-signalChannel
 }
