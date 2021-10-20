aa75ba1736965f2182dc869aa559c021064c3713
Ronen Lapushner <ronenk17@gmail.com>
2021-10-20 12:39:47 +0300

    Implemented GRPC endpoint

--- service/queryPrinterGRPC.go
+++ service/queryPrinterGRPC.go
@@ -2,9 +2,12 @@ package service
 
 import (
 	"context"
-	"log"
+	"fmt"
+	"strings"
 
 	"github.com/ronen25/task2/service/protos"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type QueryPrinterGRPCService struct {
@@ -16,11 +19,31 @@ func NewQueryPrinterGRPCService() *QueryPrinterGRPCService {
 }
 
 func (svc *QueryPrinterGRPCService) PrintParameters(ctx context.Context, r *protos.Request) (*protos.Response, error) {
-	// TODO
-	log.Printf("%v", r)
+	// Check for 3 parameters. We expect the query to be similar
+	// to the HTTP format.
+	params := make(map[string]string)
+	values := strings.Split(r.Query, "&")
+
+	if len(values) != 3 {
+		err := status.Error(codes.InvalidArgument,
+			fmt.Sprintf("Expected 3 arguments, got %d", len(values)))
+		return nil, err
+	}
+
+	// Parse parameters
+	for _, value := range values {
+		parts := strings.Split(value, "=")
+		params[parts[0]] = parts[1]
+	}
+
+	var valueParams []string
+	for _, v := range params {
+		valueParams = append(valueParams, v)
+	}
+
 	return &protos.Response{
-		Param1: "",
-		Param2: "",
-		Param3: "",
+		Param1: valueParams[0],
+		Param2: valueParams[1],
+		Param3: valueParams[2],
 	}, nil
 }
